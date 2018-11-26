3d3644528e0977da33f4ef18471e4eef6ac6fe42
Jaryn Ferguson <Jarynferguson@gmail.com>
2018-11-26 16:55:07 -0500

    First commit

---
+++ PictureAPP/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureAPP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) // this does nothing but if i delete it the app crashes 
+        {
+            // does nothing
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String imageLocation = ""; // initializes string
+            
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "jpg files(*.jpg)|*.jpg| PNG files(*.png)|*.png| All Files(*.*)|*.*"; // able to upload any type of image file 
+
+                if(dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    imageLocation = dialog.FileName; // gets image filename  and displays it to picturbox
+                    pictureBox1.ImageLocation = imageLocation;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An Error Occured.","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+    }
+}
